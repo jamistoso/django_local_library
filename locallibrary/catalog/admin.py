7b25241c26489b4a0153edf732d52099e229aa9d
jamistoso <amistoso.juancarlos@gmail.com>
2025-01-21 12:22:55 -0800

    Added index page

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -1,3 +1,49 @@
+from .models import Author, Genre, Book, BookInstance, Language
 from django.contrib import admin
 
 # Register your models here.
+
+class BookInline(admin.TabularInline):
+    model = Book
+
+# Define the admin class
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+
+
+class BooksInstanceInline(admin.TabularInline):
+    model = BookInstance
+    extra = 0
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre')
+
+    inlines = [BooksInstanceInline]
+
+
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ('id', 'book', 'status', 'due_back')
+    list_filter = ('status', 'due_back')
+
+    fieldsets = (
+        (None, {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back')
+        }),
+    )
+
+
+@admin.register(Language)
+class LanguageAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    fields = ['name']
+
+
+admin.site.register(Genre)
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(BookInstance, BookInstanceAdmin)
